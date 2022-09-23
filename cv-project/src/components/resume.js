bf52da4251bd475320d523363634f6e8a1c4a214
Andrew Dutson <Dutsandrew@gmail.com>
2022-09-23 08:43:16 -0600

    normed styling across pages

--- cv-project/src/components/resume.js
+++ cv-project/src/components/resume.js
@@ -12,20 +12,20 @@ class Resume extends Component {
     return (
       <div id='resume'>
         <div>
-          <h1 id="general-header">General Info:</h1>
+          <h1 id="general-resume-header">General Info:</h1>
           <p id='general-name' className='resume-item'>{generalInfo.name}</p>
           <p id='general-email' className='resume-item'>{generalInfo.email}</p>
           <p id='general-phone' className='resume-item'>{generalInfo.phoneNumber}</p>
         </div>
         <div>
-          <h1 id='education-header'>Education:</h1>
+          <h1 id='education-resume-header'>Education:</h1>
           <p id='education-school' className='resume-item'>{educationalExperience.schoolName}</p>
           <p id='education-degree' className='resume-item'>{educationalExperience.degree}</p>
           <p id='education-start-date' className='resume-item'>Start Date: {educationalExperience.startDate}</p>
           <p id='education-end-date' className='resume-item'>End Date: {educationalExperience.endDate}</p>
         </div>
         <div>
-          <h1 id='experience-header'>Experience:</h1>
+          <h1 id='experience-resume-header'>Experience:</h1>
           <p id='practical-company' className='resume-item'>{practicalExperience.companyName}</p>
           <p id='practical-position' className='resume-item'>{practicalExperience.positionTitle}</p>
           <p id='practical-tasks' className='resume-item'>{practicalExperience.mainTasks}</p>
